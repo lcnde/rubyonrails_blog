b4552fae54f197b543cd3827c6956d5eb7f3e938
lcnde <176673026+lcnde@users.noreply.github.com>
2020-12-29 16:37:52 +0100

    added validations in article model

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,6 @@
 class Article < ApplicationRecord
+  #validations are used to be sure that the input of the user is
+  #valid and respects the rules that we decided
+  validates :title, presence: true
+  validates :body, presence: true, length: { minimum: 10 }
 end

2e79eb48b95000c6fcdfa35f1880cf2e6578dbc2
lcnde <176673026+lcnde@users.noreply.github.com>
2020-12-29 18:26:49 +0100

    added comment section

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,6 @@
 class Article < ApplicationRecord
+  has_many :comments
+
   #validations are used to be sure that the input of the user is
   #valid and respects the rules that we decided
   validates :title, presence: true
